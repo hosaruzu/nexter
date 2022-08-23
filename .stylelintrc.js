6f85c204264b1999c5220de6b60097e25789052d
Artem Tebenkov <artem.tebenkov@icloud.com>
2022-08-23 20:06:26 +0400

    init: nexter project init

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-prettier/recommended',
+  ],
+  plugins: ['stylelint-order', 'stylelint-scss'],
+  rules: {
+    'selector-class-pattern': null,
+    'keyframes-name-pattern': null,
+    'at-rule-no-unknown': null,
+    'no-empty-source': null,
+    'scss/at-rule-no-unknown': true,
+    'no-invalid-double-slash-comments': true,
+  },
+};
